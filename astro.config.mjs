b89289a52b6d149fc780e7080d7f25c3824dff76
Tadeas Thelen <ttadeasthelen@gmail.com>
2023-07-12 22:39:38 +0200

    Remove custom deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nimixx.github.io',
-  base: '/summary-card-component',
+
 });
